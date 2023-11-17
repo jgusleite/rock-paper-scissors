e1bd6e5eb83ffdf4d29dd27d965ee0a5fe5c4d47
Jean Leite <jgusleite@gmail.com>
2023-11-17 15:33:50 -0300

    Style page.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorsBtn = document.querySelector(".scissors");
+const playerPara = document.querySelector(".player-points");
+const computerPara = document.querySelector(".computer-points");
+const winnerPara = document.querySelector(".winner");
+
 function getComputerChoice() {
   let choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
@@ -8,71 +15,50 @@ function playRound(playerSelection, computerSelection) {
   computerSelection = computerSelection.toLowerCase();
 
   if (playerSelection === "rock") {
-    if (computerSelection === "rock") return "tie";
-    if (computerSelection === "paper") return "lose";
-    if (computerSelection === "scissors") return "win";
+    if (computerSelection === "rock") return ["tie", "Rock", "Rock"];
+    if (computerSelection === "paper") return ["lose", "Rock", "Paper"];
+    if (computerSelection === "scissors") return ["win", "Rock", "Scissors"];
   }
 
   if (playerSelection === "paper") {
-    if (computerSelection === "rock") return "win";
-    if (computerSelection === "paper") return "tie";
-    if (computerSelection === "scissors") return "lose";
+    if (computerSelection === "rock") return ["win", "Paper", "Rock"];
+    if (computerSelection === "paper") return ["tie", "Paper", "Paper"];
+    if (computerSelection === "scissors") return ["lose", "Paper", "Scissors"];
   }
 
   if (playerSelection === "scissors") {
-    if (computerSelection === "rock") return "lose";
-    if (computerSelection === "paper") return "win";
-    if (computerSelection === "scissors") return "tie";
+    if (computerSelection === "rock") return ["lose", "Scissors", "Rock"];
+    if (computerSelection === "paper") return ["lose", "Scissors", "Rock"];
+    if (computerSelection === "scissors") return ["lose", "Scissors", "Rock"];
   }
 }
 
-function game() {
-  const numberOfRounds = 5;
-  let playerCounter = 0;
-  let computerCounter = 0;
+// for (let i = 0; i < numberOfRounds; i++) {
+//   let playerSelection = prompt("Your choice: ");
+//   let computerSelection = getComputerChoice();
+//   let result = playRound(playerSelection, computerSelection);
 
-  for (let i = 0; i < numberOfRounds; i++) {
-    let playerSelection = prompt("Your choice: ");
-    let computerSelection = getComputerChoice();
-    let result = playRound(playerSelection, computerSelection);
+// playerSelection =
+//   playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
 
-    playerSelection =
-      playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+// computerSelection =
+//   computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
 
-    computerSelection =
-      computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+//   console.log(playerSelection);
 
-    console.log(playerSelection);
+//   switch (result) {
+//     case "tie":
+//       console.log("Tied Game!");
+//       break;
 
-    switch (result) {
-      case "tie":
-        console.log("Tied Game!");
-        break;
+//     case "win":
+//       console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+//       playerPoints++;
+//computerPoints       break;
 
-      case "win":
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-        playerCounter++;
-        break;
-
-      case "lose":
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-        computerCounter++;
-        break;
-    }
-  }
-
-  if (playerCounter === computerCounter)
-    console.log(
-      `The game has tied. Player Wins ${playerCounter}, Computer Wins ${computerCounter}.`
-    );
-  else if (playerCounter > computerCounter)
-    console.log(
-      `Player wins with ${playerCounter} wins vs ${computerCounter} wins from the computer.`
-    );
-  else
-    console.log(
-      `Computer wins with ${computerCounter} wins vs ${playerCounter} wins from the player.`
-    );
-}
-
-game();
+//     case "lose":
+//       console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+//       computerCounter++;
+//       break;
+//   }
+// }
